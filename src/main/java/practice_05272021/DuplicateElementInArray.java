68f261fe4ea8843a860d3730c7e2d321a9f5fb9d
Venky <venkys1259@gmail.com>
2021-05-28 22:04:07 -0400

    duplicateElementsWithO(N)constantTime

--- src/main/java/practice_05272021/DuplicateElementInArray.java
+++ src/main/java/practice_05272021/DuplicateElementInArray.java
@@ -1,5 +1,23 @@
 package practice_05272021;
+/*
+https://leetcode.com/problems/find-all-duplicates-in-an-array/
+Given an integer array nums of length n where all the integers of nums are in the range [1, n] and each integer appears once or twice, return an array of all the integers that appears twice.
 
+You must write an algorithm that runs in O(n) time and uses only constant extra space.
+
+ Example 1:
+
+Input: nums = [4,3,2,7,8,2,3,1]
+Output: [2,3]
+Example 2:
+
+Input: nums = [1,1,2]
+Output: [1]
+Example 3:
+
+Input: nums = [1]
+Output: []
+ */
 import java.util.*;
 
 public class DuplicateElementInArray {
@@ -33,7 +51,7 @@ public class DuplicateElementInArray {
     }
 
     /*
-     O(N) with Constant Time.
+     O(N) with Constant space.
      */
 
     private List<Integer> findDuplicates(int[] nums) {
