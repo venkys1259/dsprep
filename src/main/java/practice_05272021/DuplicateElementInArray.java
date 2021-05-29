08162a6a7bcd8980754c6f21ffd0d2a81a4623fe
Venky <venkys1259@gmail.com>
2021-05-28 21:07:14 -0400

    duplicateElementsWithO(N)constantTime

--- src/main/java/practice_05272021/DuplicateElementInArray.java
+++ src/main/java/practice_05272021/DuplicateElementInArray.java
@@ -1,12 +1,9 @@
 package practice_05272021;
 
-import java.util.ArrayList;
-import java.util.HashMap;
-import java.util.List;
-import java.util.Map;
+import java.util.*;
 
 public class DuplicateElementInArray {
-    private List<Integer> findDuplicates(int[] nums) {
+    private List<Integer> findDup(int[] nums) {
         List<Integer> dupList = new ArrayList<> ();
         Map<Integer,Integer> numsMap = new HashMap<> ();
         int counter = 0;
@@ -20,6 +17,38 @@ public class DuplicateElementInArray {
         }
         return dupList;
     }
+
+    private List<Integer> findDups(int[] nums) {
+        List<Integer> dupList = new ArrayList<> ();
+        int index = 0;
+       for(int i = 0; i<nums.length;i++){
+           index = Math.abs(nums[i]) -1 ;
+           if(nums[index] < 0){
+               dupList.add(nums[i]);
+           }else{
+               nums[index] = -nums[index];
+           }
+       }
+        return dupList;
+    }
+
+    /*
+     O(N) with Constant Time.
+     */
+
+    private List<Integer> findDuplicates(int[] nums) {
+        List<Integer> dupList = new ArrayList<> ();
+        Arrays.sort(nums);
+        for(int i = 0; i<nums.length-1;i++){
+            if(nums[i] == nums[i+1]){
+                dupList.add(nums[i]);
+            }
+
+        }
+        return dupList;
+    }
+
+
     public static void main(String[] args) {
         int[] a = {4,3,2,7,8,2,3,1};
         List<Integer> duplicates = new DuplicateElementInArray ().findDuplicates (a);
