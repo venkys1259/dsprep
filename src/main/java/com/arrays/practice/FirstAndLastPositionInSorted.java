4ccf73dbb93751ffefd94b9c13c7d414b234e052
Venky <venkys1259@gmail.com>
2021-06-08 20:08:30 -0400

    firstAndLastPosition

---
+++ src/main/java/com/arrays/practice/FirstAndLastPositionInSorted.java
@@ -0,0 +1,80 @@
+package com.arrays.practice;
+
+import java.util.Arrays;
+
+/*
+https://leetcode.com/problems/find-first-and-last-position-of-element-in-sorted-array/
+34. Find First and Last Position of Element in Sorted Array
+Given an array of integers nums sorted in ascending order, find the starting and ending position of a given target value.
+If target is not found in the array, return [-1, -1].
+You must write an algorithm with O(log n) runtime complexity.
+Example 1:
+
+Input: nums = [5,7,7,8,8,10], target = 8
+Output: [3,4]
+Example 2:
+
+Input: nums = [5,7,7,8,8,10], target = 6
+Output: [-1,-1]
+Example 3:
+
+Input: nums = [], target = 0
+Output: [-1,-1]
+
+
+Constraints:
+
+0 <= nums.length <= 105
+-109 <= nums[i] <= 109
+nums is a non-decreasing array.
+-109 <= target <= 109
+ */
+public class FirstAndLastPositionInSorted {
+    /* 2 pointer approach
+    * iterating one pointer from start and other from end
+    * if element found respectively, just fill the array*/
+    private int[] search(int[] nums, int target){
+        int[] result = {-1,-1};
+        int end = nums.length-1;
+        boolean firstIndexFound = false, secondIndexFound = false;
+        if(nums.length == 1){
+            if(nums[0] == target){
+                result[0] = 0;
+                result[1] = 0;
+                return  result;
+            }
+        }
+        if(nums.length == 0) return  result;
+        for(int i = 0, j= end;i<end || j>0;i++,j--){
+            if(!firstIndexFound && nums[i] == target){
+                firstIndexFound=true;
+                result[0] = i;
+            }
+            if(!secondIndexFound && nums[j] == target){
+                secondIndexFound = true;
+                result[1] = j;
+            }
+            if(firstIndexFound && secondIndexFound)
+                break;
+            if(firstIndexFound){
+                i = end;
+            }
+            if(secondIndexFound){
+                j = -1;
+            }
+        }
+        if(firstIndexFound && !secondIndexFound){
+            result[1] = result[0];
+        }
+        if(!firstIndexFound && secondIndexFound){
+            result[0] = result[1];
+        }
+        return  result;
+    }
+
+    public static void main(String[] args) {
+        FirstAndLastPositionInSorted sorted = new FirstAndLastPositionInSorted();
+        int[] a = {5,7,7,8,8,10};
+        System.out.println (Arrays.toString(sorted.search(a,8)));
+    }
+}
