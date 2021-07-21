93b66b90226f53fc23f81cf24822a6e3e62fd374
Venky <venkys1259@gmail.com>
2021-07-21 18:20:34 -0400

    FindTheTownJudge

--- src/main/java/com/basics/arrays/FindTheTownJudge.java
+++ src/main/java/com/basics/arrays/FindTheTownJudge.java
@@ -1,7 +1,8 @@
 package com.basics.arrays;
 /* https://leetcode.com/problems/find-the-town-judge/ */
-import java.util.LinkedHashMap;
-import java.util.Map;
+import javafx.util.Pair;
+
+import java.util.*;
 
 public class FindTheTownJudge {
   class Trust{
@@ -19,6 +20,7 @@ public class FindTheTownJudge {
           this.trustedBy = trustedBy;
       }
   }
+  /** naive approach*/
     public int findJudge(int n, int[][] trustArr) {
       if(n == 1) return 1;
       Map<Integer,Trust> trustMap = new LinkedHashMap<> ();
@@ -49,10 +51,31 @@ public class FindTheTownJudge {
        }
         return -1;
     }
+
+    public int findJudge1(int n, int[][] trustArr) {
+        Map<Integer,Pair<Integer,Integer>> trustMap = new HashMap<> ();
+        if(n == 1) return 1;
+        for(int[] people: trustArr) {
+            Pair<Integer,Integer> trusts = trustMap.getOrDefault (people[0],new Pair(0,0));
+            Pair<Integer,Integer> trustedBy = trustMap.getOrDefault (people[1],new Pair(0,0));
+            trustMap.put(people[0], new Pair(trusts.getKey ()+1,trusts.getValue ()));
+            trustMap.put(people[1], new Pair(trustedBy.getKey(),trustedBy.getValue()+1));
+        }
+
+        for(Map.Entry<Integer,Pair<Integer,Integer>> trustEntry: trustMap.entrySet()){
+            Pair<Integer,Integer> pairs = trustEntry.getValue ();
+            if(pairs.getKey()== 0 && pairs.getValue() == n-1){
+                return trustEntry.getKey();
+            }
+        }
+      return -1;
+    }
+
     public static void main(String[] args) {
        int[][] trust = {{1,3}, {2,3}};
         FindTheTownJudge townJudge = new FindTheTownJudge();
         System.out.println (townJudge.findJudge (3, trust));
+        System.out.println (townJudge.findJudge1 (3, trust));
     }
 
 }
