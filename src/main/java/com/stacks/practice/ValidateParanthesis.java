34b5203546b7772fa9decccd15ec587a5730fa9a
Venky <venkys1259@gmail.com>
2021-07-23 08:36:03 -0400

    ValidParanthesis

---
+++ src/main/java/com/stacks/practice/ValidateParanthesis.java
@@ -0,0 +1,40 @@
+package com.stacks.practice;
+
+import java.util.*;
+
+/*
+https://leetcode.com/problems/valid-parentheses/
+ */
+public class ValidateParanthesis {
+    public boolean isValid(String s) {
+        HashMap<Character,Character> charMap = new HashMap<> ();
+        charMap.put('(',')');
+        charMap.put('[',']');
+        charMap.put('{','}');
+        Set<Character> charsSet = charMap.keySet ();
+        Collection<Character> values = charMap.values ();
+        Stack<Character> charStack = new Stack<>();
+        char[] chars = s.toCharArray ();
+        for(Character ch:chars){
+             if(charsSet.contains (ch)){
+                charStack.push(ch);
+            }else{
+                 if(charStack.isEmpty () ){
+                     charStack.push(ch);
+                 }else {
+                     if (charMap.get (charStack.pop ()) != ch) {
+                         return false;
+                     }
+                 }
+            }
+        }
+        if(!charStack.isEmpty()) return false;
+        return true;
+    }
+
+    public static void main(String[] args) {
+        ValidateParanthesis valPar = new ValidateParanthesis();
+        System.out.println (valPar.isValid ("()"));
+    }
+
+}
