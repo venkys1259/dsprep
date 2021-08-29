f42c62a4f694fe5aa0e674ee355c082a0f488fb1
Venky <venkys1259@gmail.com>
2021-08-29 14:57:15 -0400

    IsValidAnagram

---
+++ src/main/java/com/test/Anagram.java
@@ -0,0 +1,25 @@
+package com.test;
+
+import java.util.Arrays;
+
+public class Anagram {
+    public boolean isAnagram(String s, String t){
+        if(s.length () != t.length ()) return false;
+        char[] source = s.toCharArray ();
+        char[] target = t.toCharArray ();
+        Arrays.sort (source);
+        Arrays.sort(target);
+        for(int i =0;i<source.length;i++){
+            if(source[i] != target[i]){
+                return false;
+            }
+        }
+        return true;
+    }
+    public static void main(String[] args) {
+        Anagram anagram = new Anagram ();
+        System.out.println (anagram.isAnagram ("anagram","nagaram"));
+        System.out.println (anagram.isAnagram ("rat","car"));
+    }
+
+}
