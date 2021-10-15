f8c3041d927379a0f06d7ad200d1883afa83efa1
Venky <venkys1259@gmail.com>
2021-10-14 22:00:53 -0400

    PartitionEqualsSubsetSum

--- src/main/java/com/dynamicprogramming/PartitionEqualsSubsetSum.java
+++ src/main/java/com/dynamicprogramming/PartitionEqualsSubsetSum.java
@@ -17,19 +17,24 @@ public class PartitionEqualsSubsetSum {
         int n = nums.length;
         boolean[][] dp = new boolean[n+1][sum+1];
         // base condition
-        dp[0][0] = true; // sum = 0 can be achieved by not including any element in the subset.
-        for(int j = 1; j<sum;j++){
-           dp[0][j] = false; // sum = 0 cant be achieved by including 0
+        //  dp[0][0] = true; // sum = 0 can be achieved by not including any element in the subset.
+        for(int j = 0; j<=sum;j++){
+            dp[0][j] = false; // sum = 0 cant be achieved by including 0
         }
-        for(int i = 1; i<=n;i++){
+        for(int i = 0; i<=n;i++){
             dp[i][0] = true; // sum = 0 can be achieved by not including any of the elements.
             // check the picture PartitionEqualsSubsetSum.jpg for understanding.
         }
         // template of constructing dp table
         for(int i = 1; i<=n;i++){
             for(int j = 1; j <=sum;j++){
-                // include/ exclude the current element
-                dp[i][j] = dp[i-1][sum-nums[i-1]] || dp[i-1][j];
+                if(nums[i-1] <= j) {
+                    // include/ exclude the current element
+                    dp[i][j] = dp[i-1][j-nums[i-1]] || dp[i-1][j];
+                }
+                else{
+                    dp[i][j] = dp[i-1][j];
+                }
             }
         }
         return  dp[n][sum];
